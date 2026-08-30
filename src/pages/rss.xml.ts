a759addc2b4b1cc47ca63646996989cf48966228
Ruupert Koponen <ruupert.koponen@gmail.com>
2026-08-30 21:51:12 +0300

    Add rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import type { APIContext } from "astro";
+
+export async function GET(context: APIContext) {
+  const posts = (await getCollection("blog")).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+  );
+
+  return rss({
+    title: "Ruupert Koponen",
+    description: "Personal website & blog by Ruupert Koponen",
+    site: context.site!,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.id}/`,
+    })),
+  });
+}
